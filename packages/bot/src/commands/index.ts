5999bf3abddf870c02ac2790aeef5011b6d05441
Doğukan Çetin <73751643+DgknCtn@users.noreply.github.com>
2025-09-30 00:06:13 +0300

    Include additional bot commands

--- packages/bot/src/commands/index.ts
+++ packages/bot/src/commands/index.ts
@@ -1,8 +1,12 @@
 import { CommandMap } from './command.js';
+import { configCommand } from './config.js';
 import { leaderboardCommand } from './leaderboard.js';
 import { linkTwitterCommand } from './link-twitter.js';
+import { multipliersCommand } from './multipliers.js';
 import { pointsCommand } from './points.js';
+import { questCommand } from './quest.js';
 import { redeemCommand } from './redeem.js';
+import { rewardsCommand } from './rewards.js';
 import { walletCommand } from './wallet.js';
 
 export const commands = [
@@ -11,6 +15,10 @@ export const commands = [
   pointsCommand,
   leaderboardCommand,
   redeemCommand,
+  configCommand,
+  multipliersCommand,
+  questCommand,
+  rewardsCommand,
 ];
 
 export const commandMap: CommandMap = new Map(commands.map((command) => [command.data.name, command]));
